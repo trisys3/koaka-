8e5f9d98b9b208e021e4536877de38be1eda4628
Sean Moran <trysis3@gmail.com>
2017-08-03 23:36:33 -0400

    Removed the index test file as index.js is too simple to test

--- index.test.js
+++
@@ -1,8 +0,0 @@
-import {server, behavior, behaviors} from './index';
-
-jest.mock('koa');
-
-test('Server behaviors should be exposed by themselves.', () =>
-  expect(behavior).toBe(server.behavior));
-test('Behaviors should be exposed as singular or plural so users can use the one that works.', () =>
-  expect(behavior).toBe(behaviors));
