750547ea6186428b8ae509b28a6d42247d10c8b5
Sean Moran <trysis3@gmail.com>
2017-09-05 21:44:06 -0400

    Added teaching middleware

---
+++ mw/teach.js
@@ -0,0 +1,63 @@
+import Lesson from '../teach';
+
+export default (name, lessons) => {
+  if(!lessons) {
+    lessons = name;
+    name = undefined;
+  }
+  if(!Array.isArray(lessons)) {
+    if(lessons) {
+      lessons = [lessons];
+    } else {
+      lessons = [];
+    }
+  }
+
+  if(typeof name === 'string' && lessons[0]) {
+    lessons[0].name = name;
+  }
+
+  let taught = false;
+
+  return (ctx, next) => {
+    if(!Array.isArray(ctx.lessons)) {
+      ctx.lessons = [];
+    }
+
+    if(!taught) {
+      ctx.lessons.push(...lessons.map(lesson => new Lesson(lesson)));
+    }
+
+    taught = true;
+
+    if(ctx.path !== '/teach') {
+      return next();
+    }
+
+    const newLessons = [];
+
+    if(ctx.query && ctx.query.lessons) {
+      let queryLessons = ctx.query.lessons;
+
+      if(!Array.isArray(queryLessons)) {
+        queryLessons = [queryLessons];
+      }
+
+      newLessons.push(...queryLessons);
+    }
+
+    if(ctx.body && ctx.body.lessons) {
+      let bodyLessons = ctx.body.lessons;
+
+      if(!Array.isArray(bodyLessons)) {
+        bodyLessons = [bodyLessons];
+      }
+
+      newLessons.push(...bodyLessons);
+    }
+
+    ctx.lessons.push(...newLessons.map(lesson => new Lesson(lesson)));
+
+    return next();
+  };
+};
---
+++ mw/teach.test.js
@@ -0,0 +1,153 @@
+import teach from './teach';
+
+const noop = () => {
+  // empty function
+};
+
+describe('teaching', () => {
+  let ctx = {};
+
+  beforeEach(() => {
+    ctx = {};
+  });
+
+  test('no lessons is possible', () => {
+    const teacher = teach();
+    teacher(ctx, noop);
+    expect(ctx.lessons).toHaveLength(0);
+  });
+
+  describe('a single lesson', () => {
+    const lesson = {steps: 'echo \'Hello World\''};
+
+    test('is possible', () => {
+      const teacher = teach({lessons: lesson});
+      teacher(ctx, noop);
+      expect(ctx.lessons).toHaveLength(1);
+    });
+
+    test('with a name keeps the name', () => {
+      const lessonName = 'My Lesson';
+      const teacher = teach(lessonName, {lessons: lesson});
+
+      teacher(ctx, noop);
+
+      expect(ctx.lessons[0]).toHaveProperty('name', lessonName);
+    });
+
+    test('without a name is possible', () => {
+      const teacher = teach({lessons: lesson});
+      teacher(ctx, noop);
+      expect(ctx.lessons).toHaveLength(1);
+    });
+  });
+
+  test('multiple lessons is possible', () => {
+    const lessons = [{steps: 'echo \'Hello World\''}, {steps: 'google.com'}];
+    const teacher = teach(lessons);
+    teacher(ctx, noop);
+    expect(ctx.lessons).toHaveLength(2);
+  });
+
+  test('multiple times teaches the lesson multiple times', () => {
+    const step = 'echo \'Hello world!\'';
+
+    const teacher1 = teach({steps: step});
+    const teacher2 = teach({steps: step});
+
+    teacher1(ctx, noop);
+    teacher2(ctx, noop);
+
+    expect(ctx.lessons).toHaveLength(2);
+  });
+
+  describe('with a single request', () => {
+    test('multiple times only teaches the original request once', () => {
+      const step = 'google.com';
+      const teacher = teach({steps: step});
+
+      teacher(ctx, noop);
+      teacher(ctx, noop);
+
+      expect(ctx.lessons).toHaveLength(1);
+    });
+
+    test('does not work without the /teach route', () => {
+      const step = 'google.com';
+      const teacher = teach();
+
+      ctx.path = '/';
+      ctx.query = {lessons: {steps: step}};
+      ctx.body = {lessons: {steps: step}};
+
+      teacher(ctx, noop);
+      expect(ctx.lessons).toHaveLength(0);
+    });
+
+    test('works with the /teach route', () => {
+      const step = 'google.com';
+      const teacher = teach();
+
+      ctx.path = '/teach';
+      ctx.query = {lessons: {steps: step}};
+
+      teacher(ctx, noop);
+      expect(ctx.lessons).toHaveLength(1);
+    });
+
+    test('with no lessons teaches nothing', () => {
+      const teacher = teach();
+      ctx.path = '/teach';
+      teacher(ctx, noop);
+      expect(ctx.lessons).toHaveLength(0);
+    });
+
+    describe('with a single lesson', () => {
+      test('in the query string is possible', () => {
+        const step = 'google.com';
+        const teacher = teach();
+
+        ctx.path = '/teach';
+        ctx.query = {lessons: {steps: step}};
+
+        teacher(ctx, noop);
+        expect(ctx.lessons).toHaveLength(1);
+      });
+
+      test('in the request body is possible', () => {
+        const step = 'google.com';
+        const teacher = teach();
+
+        ctx.path = '/teach';
+        ctx.body = {lessons: {steps: step}};
+
+        teacher(ctx, noop);
+        expect(ctx.lessons).toHaveLength(1);
+      });
+    });
+
+    describe('with multiple lessons', () => {
+      test('in the query string is possible', () => {
+        const step = 'google.com';
+        const teacher = teach();
+
+        ctx.path = '/teach';
+        ctx.query = {lessons: [{steps: step}, {steps: step}]};
+
+        teacher(ctx, noop);
+        expect(ctx.lessons).toHaveLength(2);
+      });
+
+      test('in the request body is possible', () => {
+        const step = 'google.com';
+        const teacher = teach();
+
+        ctx.path = '/teach';
+        ctx.body = {lessons: [{steps: step}, {steps: step}]};
+
+        teacher(ctx, noop);
+        expect(ctx.lessons).toHaveLength(2);
+      });
+    });
+  });
+});
