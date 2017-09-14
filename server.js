dd87d01374ba9a80a00f82520f174c833fb885f5
Sean Moran <trysis3@gmail.com>
2017-09-14 14:59:03 -0400

    Added a simple koa server using the teach middleware & some other middleware

---
+++ server.js
@@ -0,0 +1,62 @@
+import Koa from 'koa';
+
+import teach from './mw/teach';
+import vhost from './vhost';
+import mw from './middleware';
+
+const servers = {};
+class Koaka {
+  ports = [];
+
+  constructor({steps, domain = 'loalhost', ports} = {}) {
+    if(!Array.isArray(ports)) {
+      ports = [ports];
+    }
+
+    const koa = new Koa();
+
+    koa.use(vhost(domain))
+      .use(teach(steps))
+      .use(mw);
+
+    Object.assign(this, {
+      teach,
+      server: koa,
+      steps,
+    });
+
+    this.serve(ports);
+  }
+
+  serve(ports) {
+    if(!Array.isArray(ports)) {
+      ports = [ports];
+    }
+
+    return ports
+      .filter(port => typeof port === 'number' &&
+        !this.ports.find(servedPort => port === servedPort))
+      .map(port => {
+        this.ports.push(port);
+        return servers[port] = this.server.listen(port);
+      });
+  }
+
+  kill(ports) {
+    if(!Array.isArray(ports)) {
+      ports = [ports];
+    }
+
+    return ports
+      .filter(port => typeof port === 'number' &&
+        this.ports.find(killPort => port === killPort))
+      .map((port, portIndex) => {
+        const server = servers[port];
+        server.close();
+        this.ports.splice(portIndex, 1);
+        return server;
+      });
+  }
+}
+
+export default Koaka;
---
+++ server.test.js
@@ -0,0 +1,77 @@
+jest.mock('./mw/teach');
+jest.mock('./vhost');
+jest.mock('./middleware');
+
+import Koaka from './server';
+
+describe('A Koaka server', () => {
+  test('can serve on 0 ports at a time', () => {
+    const server = new Koaka();
+    server.serve();
+    expect(server.ports).toHaveLength(0);
+  });
+  
+  test('can serve on 1 port at a time', () => {
+    const port = 3000;
+    const server = new Koaka();
+    server.serve(port);
+    expect(server.ports).toHaveLength(1);
+  });
+  
+  test('can serve on multiple ports at a time', () => {
+    const ports = [3000, 4000];
+    const server = new Koaka();
+    server.serve(ports);
+    expect(server.ports).toHaveLength(2);
+  });
+
+  test('can not serve on ports that have already been served on', () => {
+    const port = 3000;
+    const server = new Koaka({ports: port});
+    server.serve(port);
+    expect(server.ports).toHaveLength(1);
+  });
+  
+  test('can kill 0 servers at a time', () => {
+    const ports = [
+      3000,
+      4000,
+      5000,
+    ];
+    const server = new Koaka({ports});
+    server.kill();
+    expect(server.ports).toHaveLength(3);
+  });
+  
+  test('can kill 1 server at a time', () => {
+    const ports = [
+      3000,
+      4000,
+      5000,
+    ];
+    const killPort = 3000;
+    const server = new Koaka({ports});
+    server.kill(killPort);
+    expect(server.ports).toHaveLength(2);
+  });
+  
+  test('can kill multiple servers at a time', () => {
+    const ports = [
+      3000,
+      4000,
+      5000,
+    ];
+    const killPorts = [3000, 4000];
+    const server = new Koaka({ports});
+    server.kill(killPorts);
+    expect(server.ports).toHaveLength(1);
+  });
+
+  test('can not kill ports that are not being served on', () => {
+    const port = 3000;
+    const killPort = 4000;
+    const server = new Koaka({ports: port});
+    server.kill(killPort);
+    expect(server.ports).toHaveLength(1);
+  });
+});
