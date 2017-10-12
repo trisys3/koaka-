8ff815a065a5cd618654b6cf36565930bffc5859
Sean Moran <trysis3@gmail.com>
2017-10-12 07:36:02 -0400

    Added/changed tests for the quiz middleware & added the actual middleware

--- src/mw/quiz.js
+++ src/mw/quiz.js
@@ -1,4 +1,88 @@
-export default () => {
-  return (ctx, next) => {
+import {run as runCmd} from 'node-run-cmd';
+import request from 'request-promise-native';
+
+import {isUrl, isCommand} from '../utils';
+
+export default () =>
+  async (ctx, next) => {
+    const {app, path} = ctx;
+    const lessons = app.lessons = app.lessons || {};
+
+    let steps;
+    for(const lesson in lessons) {
+      if(path === `/${lesson}`) {
+        steps = lessons[lesson];
+        break;
+      }
+    }
+
+    if(!Array.isArray(steps)) {
+      if(steps) {
+        steps = [steps];
+      } else {
+        steps = [];
+      }
+      if(!steps.length) {
+        return next();
+      }
+    }
+
+    const lessonOut = ctx.body.lessons = [];
+    const lessonPromises = steps.map((step, stepIndex) => new Promise(async (resolve, reject) => {
+      let shell;
+      let type;
+
+      if(typeof step !== 'string') {
+        ({type, shell, step} = step);
+      }
+
+      if(!type) {
+        if(isUrl(step)) {
+          type = 'url';
+        } else if(isCommand(step)) {
+          type = 'command';
+        }
+      }
+
+      switch(type) {
+        case 'command': {
+          const util = require('util');
+          const exec = util.promisify(require('child_process').exec);
+          if(shell) {
+            step = `${shell} -c "${step}"`;
+          }
+
+          try {
+            const {stdout} = await exec(step);
+            lessonOut[stepIndex] = stdout;
+            return resolve(stdout);
+          } catch({stderr}) {
+            lessonOut[stepIndex] = stderr;
+            return reject(stderr);
+          }
+        } case 'url': {
+          if(!step.match(/^https?:\/\//)) {
+            step = `http://${step}`;
+          }
+
+          try {
+            const resp = await request(step);
+            lessonOut[stepIndex] = resp;
+            return resolve(resp);
+          } catch(error) {
+            lessonOut[stepIndex] = error;
+            return reject(error);
+          }
+        } default: {
+          reject(new Error('Can\'t quiz steps other than commands and URL\'s'));
+        }
+      }
+    }));
+
+    try {
+      await Promise.all(lessonPromises);
+      ctx.status = 200;
+    } catch(error) {}
+
+    return next();
   };
-};
--- src/mw/quiz.test.js
+++ src/mw/quiz.test.js
@@ -1,11 +1,6 @@
-jest.mock('request-promise-native');
-
-// use quiz because jest is hogging the global test variable
 import quizzer from './quiz';
-import * as nrc from 'node-run-cmd';
-import request from 'request-promise-native';
+import Koa from 'koa';
 
-const run = jest.spyOn(nrc, 'run');
 const quiz = quizzer();
 
 const noop = () => {
@@ -16,115 +11,173 @@ const ctx = {};
 describe('Testing', () => {
   describe('a lesson', () => {
     beforeEach(() => {
-      Object.assign(ctx, {app: {}, status: 404});
+      Object.assign(ctx, {app: {}, body: {}, status: 404});
     });
 
     test('which does not exist fails', () => {
-      ctx.path = '/My Nonexistent Lesson';
+      const stepName = 'My Nonexistent Lesson';
+      ctx.path = `/${stepName}`;
 
       quiz(ctx, noop);
       expect(ctx.status).toBe(404);
     });
 
     test('which exists but has no steps fails', () => {
-      ctx.app.lessons = {'My Existing Lesson': []};
-      ctx.path = 'My Existing Lesson';
+      const stepName = 'My Existing Lesson';
+      ctx.path = `/${stepName}`;
 
+      ctx.app = {lessons: {[stepName]: []}};
       quiz(ctx, noop);
       expect(ctx.status).toBe(404);
     });
   });
 
   describe('a command', () => {
-    const command = 'echo Hello world!';
+    const command = 'echo -n Hello world!';
+    const output = 'Hello world!';
     const failCommand = 'cat nonexistent-file';
 
     beforeEach(() => {
       Object.assign(ctx, {app: {lessons: {}}, status: 404});
-      run.mockClear();
     });
 
-    test('calls the command', () => {
-      ctx.app.lessons['My Command Test'] = command;
-      ctx.path = '/My Command Test';
+    test('calls the command', async () => {
+      const stepName = 'My Command Test';
+      ctx.path = `/${stepName}`;
 
-      quiz(ctx, noop);
-      expect(run).toHaveBeenCalledWith(command);
+      ctx.app.lessons[stepName] = command;
+      await quiz(ctx, noop);
+      expect(ctx.body.lessons).toContain(output);
+
+      ctx.app.lessons[stepName] = {step: command};
+      await quiz(ctx, noop);
+      return expect(ctx.body.lessons).toContain(output);
     });
 
-    test('can succeed', () => {
-      ctx.app.lessons['My Succeeding Command Test'] = command;
-      ctx.path = '/My Succeeding Command Test';
+    test('can use a different shell', async () => {
+      const stepName = 'My Command Test With Different Shell';
+      ctx.path = `/${stepName}`;
 
-      quiz(ctx, noop);
+      ctx.app.lessons[stepName] = {shell: 'bash', step: command};
+      await quiz(ctx, noop);
+      return expect(ctx.body.lessons).toContain(output);
+    });
+
+    test('can succeed', async () => {
+      const stepName = 'My Succeeding Command Test';
+      ctx.path = `/${stepName}`;
+
+      ctx.app.lessons[stepName] = command;
+      await quiz(ctx, noop);
       expect(ctx.status).toBe(200);
+
+      ctx.app.lessons[stepName] = {step: command};
+      await quiz(ctx, noop);
+      return expect(ctx.status).toBe(200);
     });
 
-    test('can fail', () => {
-      ctx.app.lessons['My Failing Command Test'] = failCommand;
-      ctx.path = '/My Failing Command Test';
+    test('can fail', async () => {
+      const stepName = 'My Failing Command Test';
+      ctx.path = `/${stepName}`;
 
-      quiz(ctx, noop);
+      ctx.app.lessons[stepName] = failCommand;
+      await quiz(ctx, noop);
       expect(ctx.status).toBe(404);
+
+      ctx.app.lessons[stepName] = {step: failCommand};
+      await quiz(ctx, noop);
+      return expect(ctx.status).toBe(404);
     });
   });
 
   describe('a URL', () => {
-    const url = 'google.com';
-    const failUrl = 'koaka.io/nonexistent-path';
+    const url = 'github.com/trisys3/koaka-thing';
+    const fullUrl = 'https://github.com/koaka-thing';
+    const failUrl = 'https://github.com/nonexistent-path';
 
     beforeEach(() => {
       Object.assign(ctx, {app: {lessons: {}}, status: 404});
-      request.mockClear();
     });
 
-    test('requests the URL', () => {
-      ctx.app.lessons['My Request Test'] = url;
-      ctx.path = '/My Request Test';
+    // revisit this when we have a URL where we can be sure of the response
+    test.skip('requests the URL', async () => {
+      const stepName = 'My Request Test';
+      ctx.path = `/${stepName}`;
 
-      quiz(ctx, noop);
-      expect(request).toHaveBeenCalledWith(url);
+      ctx.app.lessons[stepName] = fullUrl;
+      await quiz(ctx, noop);
+      expect(ctx.body.lessons).toContain(fullUrl);
+
+      ctx.app.lessons[stepName] = {step: fullUrl};
+      await quiz(ctx, noop);
+      return expect(ctx.body.lessons).toContain(fullUrl);
     });
 
-    test('can succeed', () => {
-      ctx.app.lessons['My Succeeding Request Test'] = url;
-      ctx.path = '/My Succeeding Request Test';
+    // revisit this when we have a URL where we can be sure of the response
+    test.skip('requests the full URL', async () => {
+      const stepName = 'My Full URL Test';
+      ctx.path = `/${stepName}`;
 
-      quiz(ctx, noop);
+      ctx.app.lessons[stepName] = url;
+      await quiz(ctx, noop);
+      expect(ctx.body.lessons).toContain(`http://${url}`);
+
+      ctx.app.lessons[stepName] = {step: url};
+      await quiz(ctx, noop);
+      return expect(ctx.body.lessons).toContain(`http://${url}`);
+    });
+
+    test('can succeed', async () => {
+      const stepName = 'My Succeeding Request Test';
+      ctx.path = `/${stepName}`;
+
+      ctx.app.lessons[stepName] = url;
+      await quiz(ctx, noop);
       expect(ctx.status).toBe(200);
+
+      ctx.app.lessons[stepName] = {step: url};
+      await quiz(ctx, noop);
+      return expect(ctx.status).toBe(200);
     });
 
-    test('can fail', () => {
-      ctx.app.lessons['My Failing Request Test'] = failUrl;
-      ctx.path = '/My Failing Request Test';
+    test('can fail', async () => {
+      const stepName = 'My Failing Request Test';
+      ctx.path = `/${stepName}`;
 
-      quiz(ctx, noop);
+      ctx.app.lessons[stepName] = failUrl;
+      await quiz(ctx, noop);
       expect(ctx.status).toBe(404);
+
+      ctx.app.lessons[stepName] = {step: failUrl};
+      await quiz(ctx, noop);
+      return expect(ctx.status).toBe(404);
     });
   });
 
   describe('multiple commands &/or requests', () => {
-    const successSteps = ['google.com', 'echo "Hello world!"'];
-    const failSteps = ['google.com', 'cat nonexistent-file'];
+    const successSteps = ['github.com/trisys3/koaka-thing', 'echo Hello world!'];
+    const failSteps = ['github.com/trisys3/koaka-thing', 'cat nonexistent-file'];
 
     beforeEach(() => {
       Object.assign(ctx, {app: {lessons: {}}, status: 404});
     });
 
-    test('succeeds if all steps succeed', () => {
-      ctx.app.lessons['My Succeeding Test'] = successSteps;
-      ctx.path = '/My Succeeding Test';
+    test('succeeds if all steps succeed', async () => {
+      const stepName = 'My Succeeding Test';
+      ctx.app.lessons[stepName] = successSteps;
+      ctx.path = `/${stepName}`;
 
-      quiz(ctx, noop);
-      expect(ctx.status).toBe(200);
+      await quiz(ctx, noop);
+      return expect(ctx.status).toBe(200);
     });
 
-    test('fails if even 1 of the steps fails', () => {
-      ctx.app.lessons['My Failing Test'] = failSteps;
-      ctx.path = '/My Failing Test';
+    test('fails if even 1 of the steps fails', async () => {
+      const stepName = 'My Failing Test';
+      ctx.app.lessons[stepName] = failSteps;
+      ctx.path = `/${stepName}`;
 
-      quiz(ctx, noop);
-      expect(ctx.status).toBe(404);
+      await quiz(ctx, noop);
+      return expect(ctx.status).toBe(404);
     });
   });
 });
